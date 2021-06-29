c01f906c3ecd9916c241b894f0ed16849425b098
nhandang9594 <tamnhan.dang9594@gmail.com>
2021-06-29 15:14:32 -0500

    Initial Commit

--- app/src/main/java/com/example/weatherappcsce3444/MainActivity.java
+++ app/src/main/java/com/example/weatherappcsce3444/MainActivity.java
@@ -16,6 +16,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,7 +52,52 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("result",response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String dt = jsonObject.getString("dt");
+                            String name = jsonObject.getString("name");
+                            txtName.setText("City: " + name);
+
+                            long l = Long.valueOf(dt);
+                            Date date = new Date(l * 1000L);
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE yyyy-MM-dd HH-mm-ss");
+                            String day = simpleDateFormat.format(date);
+
+                            txtUpdatedDay.setText(day);
+                            JSONArray jsonArrayWeather = jsonObject.getJSONArray("weather");
+                            JSONObject jsonObjectWeather = jsonArrayWeather.getJSONObject(0);
+                            String status = jsonObjectWeather.getString("main");
+                            String icon = jsonObjectWeather.getString("icon");
+
+                            Picasso.with(MainActivity.this).load("http://openweathermap.org/img/wn/" + icon + ".png").into(imgIcon);
+                            txtStatus.setText(status);
+
+                            JSONObject jsonObjectMain = jsonObject.getJSONObject("main");
+                            String temp = jsonObjectMain.getString("temp");
+                            String humid = jsonObjectMain.getString("humidity");
+
+                            Double t = Double.valueOf(temp);
+                            String Temp = String.valueOf(t.intValue());
+
+                            txtTemp.setText(Temp+ " °F");
+                            txtHumidity.setText(humid + "%");
+
+                            JSONObject jsonObjectWind = jsonObject.getJSONObject("wind");
+                            String speed = jsonObjectWind.getString("speed");
+                            txtMill.setText(speed + " mi/h");
+
+                            JSONObject jsonObjectClouds = jsonObject.getJSONObject("clouds");
+                            String cloud = jsonObjectClouds.getString("all");
+                            txtCloud.setText(cloud + "%");
+
+                            JSONObject jsonObjectSys = jsonObject.getJSONObject("sys");
+                            String country = jsonObjectSys.getString("country");
+                            txtCountry.setText("City: " + country);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener() {

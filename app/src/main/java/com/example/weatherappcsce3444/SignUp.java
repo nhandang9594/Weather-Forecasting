523d63079ffaa915d856cd899df38cf1bf7125c8
mbertuccimobile <m.bertucciphone@gmail.com>
2021-07-19 14:15:44 -0500

    additional login verification work
    Continued working on login/registration verification

--- app/src/main/java/com/example/weatherappcsce3444/SignUp.java
+++ app/src/main/java/com/example/weatherappcsce3444/SignUp.java
@@ -3,12 +3,85 @@ package com.example.weatherappcsce3444;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class SignUp extends AppCompatActivity {
 
+    TextInputEditText  textInputEditTextfullname,
+                       textInputEditTextUsername,
+                       textInputEditTextPassword,
+                       textInputEditTextEmail;
+    Button buttonSignUp;
+    TextView textViewLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Buttons IDs
+        textInputEditTextEmail = findViewById(R.id.email);
+        textInputEditTextPassword = findViewById(R.id.password);
+        textInputEditTextfullname = findViewById(R.id.fullname);
+        textInputEditTextUsername = findViewById(R.id.username);
+
+        // button listener for sign up
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // variables for info
+                String fullname, username, password, email;
+
+                fullname = String.valueOf(textInputEditTextfullname.getText());
+                username = String.valueOf( textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+
+                if(!fullname.equals("") && !username.equals("") && !password.equals("") && !email.equals("")) {
+
+
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "username";
+                            field[2] = "password";
+                            field[3] = "email";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = "fullname";
+                            data[1] = "username";
+                            data[2] = "password";
+                            data[3] = "email";
+                            PutData putData = new PutData("http://localhost/LoginRegister/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else
+                {   // display error message
+                    Toast.makeText(getApplicationContext(),"All fields are required",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }

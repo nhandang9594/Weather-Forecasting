a5b85bb9f805e864743655b095cbceaf597efcdc
mbertuccimobile <m.bertucciphone@gmail.com>
2021-08-02 05:52:57 -0500

    Polished up errors, and tested

--- app/src/main/java/com/example/weatherappcsce3444/Login.java
+++ app/src/main/java/com/example/weatherappcsce3444/Login.java
@@ -2,13 +2,113 @@ package com.example.weatherappcsce3444;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Login extends AppCompatActivity {
 
+    TextInputEditText textInputEditTextUsername, textInputEditTextPassword;
+
+    Button buttonLogin;
+    TextView textViewSignUp;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textInputEditTextUsername = findViewById(R.id.username);
+        textInputEditTextPassword = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.btnLogin);
+        progressBar = findViewById(R.id.progress);
+        textViewSignUp = findViewById(R.id.signUpText);
+
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // progress bar
+        progressBar = findViewById(R.id.progress);
+
+        // button listener for sign up
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // variables for info
+                String  username, password;
+
+                username = String.valueOf( textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+
+                if(!username.equals("") && !password.equals("") ) {
+                    //Start ProgressBar first (Set visibility VISIBLE)
+               progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+
+                            field[0] = "username";
+                            field[1] = "password";
+
+                            //Creating array for data
+                            String[] data = new String[2];
+
+                            data[0] = username;
+                            data[1] = password;
+
+                            PutData putData = new PutData("http://192.168.254.25/LoginRegister/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Login Success")) {
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else
+                {   // display error message
+                    Toast.makeText(getApplicationContext(),"All fields are required",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/weatherappcsce3444/SignUp.java
+++ app/src/main/java/com/example/weatherappcsce3444/SignUp.java
@@ -2,10 +2,12 @@ package com.example.weatherappcsce3444;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,12 +16,15 @@ import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class SignUp extends AppCompatActivity {
 
-    TextInputEditText  textInputEditTextfullname,
+    TextInputEditText  textInputEditTextFullname,
                        textInputEditTextUsername,
                        textInputEditTextPassword,
                        textInputEditTextEmail;
     Button buttonSignUp;
     TextView textViewLogin;
+    ProgressBar progressBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +33,23 @@ public class SignUp extends AppCompatActivity {
         // Buttons IDs
         textInputEditTextEmail = findViewById(R.id.email);
         textInputEditTextPassword = findViewById(R.id.password);
-        textInputEditTextfullname = findViewById(R.id.fullname);
+        textInputEditTextFullname = findViewById(R.id.fullname);
         textInputEditTextUsername = findViewById(R.id.username);
+
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        textViewLogin = findViewById(R.id.loginText);
+
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // progress bar
+        progressBar = findViewById(R.id.progress);
 
         // button listener for sign up
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
@@ -38,15 +58,14 @@ public class SignUp extends AppCompatActivity {
                 // variables for info
                 String fullname, username, password, email;
 
-                fullname = String.valueOf(textInputEditTextfullname.getText());
+                fullname = String.valueOf(textInputEditTextFullname.getText());
                 username = String.valueOf( textInputEditTextUsername.getText());
                 password = String.valueOf(textInputEditTextPassword.getText());
                 email = String.valueOf(textInputEditTextEmail.getText());
 
                 if(!fullname.equals("") && !username.equals("") && !password.equals("") && !email.equals("")) {
-
-
                     //Start ProgressBar first (Set visibility VISIBLE)
+                    progressBar.setVisibility(View.VISIBLE);
                     Handler handler = new Handler();
                     handler.post(new Runnable() {
                         @Override
@@ -60,14 +79,26 @@ public class SignUp extends AppCompatActivity {
                             field[3] = "email";
                             //Creating array for data
                             String[] data = new String[4];
-                            data[0] = "fullname";
-                            data[1] = "username";
-                            data[2] = "password";
-                            data[3] = "email";
-                            PutData putData = new PutData("http://localhost/LoginRegister/signup.php", "POST", field, data);
+                            data[0] = fullname;
+                            data[1] = username;
+                            data[2] = password;
+                            data[3] = email;
+                            PutData putData = new PutData("http://192.168.254.25/LoginRegister/signup.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
                                     String result = putData.getResult();
+                                    if(result.equals("Sign Up Success")) {
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(),Login.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
 
                                 }
                             }
